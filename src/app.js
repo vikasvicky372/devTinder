8a8d34187504890bd6732f7b7f1c80c43f072270
Vikas Thulasi <vikasvicky372@gmail.com>
2024-11-28 00:46:20 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -10,8 +10,45 @@ const data = {
 }
 //request handlers
 
-app.use("/user",(req,res) => {
+// app.use("/hello", (req,res) => {
+//     res.send("hello");
+// });
+
+// app.use("/hello2", (req,res) => {
+//     res.send("hello2");
+// });
+
+// app.use("/hello/2", (req,res) => {
+//     res.send("hello/2");
+// });
+
+
+
+// /user?userId=123  req.query = { userId: '1234' }
+// /user?userId=1234&password=vikas   { userId: '1234', password: 'vikas' }
+app.get("/user",(req,res) => {
+    console.log(req.query);
+    res.send("success");
+});
+//  /user/1234/vikas/pass   req.params = { userId: '1234', name: 'vikas', password: 'pass' }
+app.get("/user/:userId/:name/:password", (req,res) => {
+    console.log(req.params);
+    res.send(req.params);
+})
+
+app.get("/user",(req,res) => {
     res.send(JSON.stringify(data));
+});
+
+app.post("/user",async (req,res) => {
+    const body =  req.body;
+    console.log(body);
+    res.send("success");
+})
+
+//any url that contains a  /cab , /cat , /ab , /aa , /a
+app.get(/a/, (req,res) => {
+    res.send("hello");
 });
 
 app.use("/",(req,res) => {
