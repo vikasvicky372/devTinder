798e56f436dfe4dc4c4079287ab6fddfd21ea3e3
Vikas Thulasi <vikasvicky372@gmail.com>
2024-12-01 23:20:27 +0530

    find users

--- src/app.js
+++ src/app.js
@@ -9,25 +9,65 @@ app.use(express.json());
 
 app.post("/signUp", async (req,res) => {
 
-    console.log(req.body);
-    const user = new User(req.body);
+console.log(req.body);
+const user = new User(req.body);
 
-    try{
-      await user.save(); 
-      res.send("user saved successfully");
-    } catch (err) {
-        res.status(400).send("error occured while saving the user"+ err);
-    }
+try{
+await user.save(); 
+res.send("user saved successfully");
+} catch (err) {
+res.status(400).send("error occured while saving the user"+ err);
+}
+})
+
+app.get("/user", async (req,res) => {
+const userEmail = req?.body?.emailId;
+
+try{
+const users = await User.find({emailId: userEmail});
+if(users.length ===0){
+    res.status(404).send("User not found");
+} else{
+    res.send(users);
+}
+} catch(err) {
+res.status(500).send("Something went wrong");
+}
+
+})
+
+app.get("/feed", async (req,res) => {
+
+try {
+    const users = await User.find({});
+    res.send(users);
+}catch(err) {
+    res.status(500).send("Something went wrong");
+}
+})
+
+app.get("/oneUser", async (req,res) => {
+const userEmail = req?.body?.emailId;
+
+try{
+    const user = await User.findOne({emailId: userEmail});
+    
+        res.send(user);
+
+} catch(err) {
+    res.status(500).send("Something went wrong");
+}
+
 })
 
 connectDB().then(()=>{
 console.log("database connected succesfully")
 app.listen(1234, () => {
-    console.log("server is succesfully running on port 1234");
+console.log("server is succesfully running on port 1234");
 });
 }).catch(
 err => {
-    console.log("database connection is not established..")
+console.log("database connection is not established..")
 }
 )
 
