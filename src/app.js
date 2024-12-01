fe646a4ce2f41009222f621df60dd35c86444deb
Vikas Thulasi <vikasvicky372@gmail.com>
2024-12-01 17:10:22 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 //creating server from express
 const app = express();
 
+const {adminAuth,userAuth} = require("./middlewares/auth");
+
 const data = {
     author: "Vikas Thulasi",
     app: "DevTinder"
@@ -26,34 +28,50 @@ const data = {
 
 // /user?userId=123  req.query = { userId: '1234' }
 // /user?userId=1234&password=vikas   { userId: '1234', password: 'vikas' }
-app.get("/user",(req,res) => {
-    console.log(req.query);
-    res.send("success");
+// app.get("/user",(req,res) => {
+//     console.log(req.query);
+//     res.send("success");
+// });
+// //  /user/1234/vikas/pass   req.params = { userId: '1234', name: 'vikas', password: 'pass' }
+// app.get("/user/:userId/:name/:password", (req,res) => {
+//     console.log(req.params);
+//     res.send(req.params);
+// })
+
+// app.get("/user",(req,res) => {
+//     res.send(JSON.stringify(data));
+// });
+
+// app.post("/user",async (req,res) => {
+//     const body =  req.body;
+//     console.log(body);
+//     res.send("success");
+// })
+
+// //any url that contains a  /cab , /cat , /ab , /aa , /a
+// app.get(/a/, (req,res) => {
+//     res.send("hello");
+// });
+
+// app.use("/",(req,res) => {
+//     res.send("Hello from DevTinder");
+// });
+
+
+app.use("/admin",adminAuth);
+
+
+app.get("/admin/getAllData", (req,res,next) => {
+    res.send("All data returned");
 });
-//  /user/1234/vikas/pass   req.params = { userId: '1234', name: 'vikas', password: 'pass' }
-app.get("/user/:userId/:name/:password", (req,res) => {
-    console.log(req.params);
-    res.send(req.params);
+
+app.get("/admin/deleteUser", (req,res,next) => {
+    res.send("deleted a user");
+});
+
+app.get("/user", userAuth, (req,res) => {
+    res.send("User data")
 })
-
-app.get("/user",(req,res) => {
-    res.send(JSON.stringify(data));
-});
-
-app.post("/user",async (req,res) => {
-    const body =  req.body;
-    console.log(body);
-    res.send("success");
-})
-
-//any url that contains a  /cab , /cat , /ab , /aa , /a
-app.get(/a/, (req,res) => {
-    res.send("hello");
-});
-
-app.use("/",(req,res) => {
-    res.send("Hello from DevTinder");
-});
 
 // listening to the incoming requests
 
---
+++ src/middlewares/auth.js
@@ -0,0 +1,24 @@
+const adminAuth = (req,res,next) => {
+    const token = "xyz";
+    const isAuthorized = token === "xyz";
+    if(!isAuthorized){
+        res.status(401).send("Unauthorized request");
+    } else {
+        next();
+    }
+};
+
+const userAuth = (req,res,next) => {
+    const token = "xyza";
+    const isAuthorized = token === "xyz";
+    if(!isAuthorized){
+        res.status(401).send("Unauthorized request");
+    } else {
+        next();
+    }
+};
+
+module.exports = {
+    adminAuth,
+    userAuth
+}
