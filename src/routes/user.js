554b8bf5021d8088f27bb69cb9b778fcb81877cd
Vikas Thulasi <vikasvicky372@gmail.com>
2025-05-16 17:21:22 +0530

    feed API added

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -44,6 +45,43 @@ userRouter.get("/user/connections", userAuth, async(req, res) => {
         });
         res.json({
             data
+        });
+
+    }catch(err){
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+userRouter.get("/user/feed", userAuth, async(req, res) => {
+    try{
+        //get all user except the logged in user
+        //get all users who are not in the connection list
+        //get all the user who are previously rejected or accepted
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit>50? 50 : limit;
+        const skip = (page - 1) * limit;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach(row => {
+            hideUsersFromFeed.add(row.fromUserId.toString());
+            hideUsersFromFeed.add(row.toUserId.toString());
+        });
+
+        const filteredUsers = await User.find({
+            $and:[{_id:{ $nin: Array.from(hideUsersFromFeed) }},
+                {_id: { $ne: loggedInUser._id }}
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+        res.status(200).json({
+            data:filteredUsers
         });
 
     }catch(err){
