b0f562ce6d8b319cc2a0bf3411244343c6696012
Vikas Thulasi <vikasvicky372@gmail.com>
2025-05-16 21:49:42 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -10,13 +10,19 @@ const cookieParser = require("cookie-parser");
 const {createJwtToken} = require("./utils/jwtToken");
 const { userAuth } = require("./middlewares/auth");
 const user = require("./models/user");
-app.use(express.json());
-app.use(cookieParser());
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const cors = require("cors");
 
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
 //using the routes
 app.use("/", authRouter);
 app.use("/", profileRouter);
