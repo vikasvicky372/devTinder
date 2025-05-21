540c9c8fe1a8e5df114f7a7b3a1743ea329538ff
Vikas Thulasi <vikasvicky372@gmail.com>
2025-05-21 19:19:01 +0530

    scheduling cron job

--- src/utils/cronJob.js
+++ src/utils/cronJob.js
@@ -3,16 +3,13 @@ const connectionModel = require('../models/connectionRequest');
 const {subDays, startOfDay, endOfDay} = require("date-fns")
 const sendEmail = require('../utils/sendEmail');
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 8 * * *', async () => {
     // sending emails to users
 
     try{
         const yesterdayDate = subDays(new Date(), 1);
-        console.log("Yesterday date: ", yesterdayDate);
         const yesterdayStart = startOfDay(yesterdayDate);
-        console.log("Yesterday start: ", yesterdayStart);
         const yesterdayEnd = endOfDay(yesterdayDate);
-        console.log("Yesterday end: ", yesterdayEnd);
         const requests = await connectionModel.find({
             status: "interested",
             createdAt: {
